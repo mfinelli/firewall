8e77f31ddd7c8d23bfb2d1664105965e630c1589
Mario Finelli <mario@finel.li>
2015-02-19 22:02:08 -0500

    Add a new script to help prevent port scanning.
    This is an optional section of the guide on the wiki but it's very easy
    to implement and has been added in an option way here as well.

---
+++ portscanner.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Get include path. (http://stackoverflow.com/a/12694189)
+dir="${BASH_SOURCE%/*}"
+if [[ ! -d "$dir" ]]; then
+    dir="$PWD"
+fi
+
+# Include functions.
+source "$dir"/include.sh
+
+# Make sure that we have permission.
+check_root
+
+# Make sure that iptables is actually installed.
+check_iptables
+
+# syn scans.
+iptables -I TCP -p tcp -m recent --update --seconds 60 --name TCP-PORTSCAN -j REJECT --reject-with tcp-rst
+iptables -D INPUT -p tcp -j REJECT --reject-with tcp-rst
+iptables -A INPUT -p tcp -m recent --set --name TCP-PORTSCAN -j REJECT --reject-with tcp-rst
+
+# udp scans.
+iptables -I UDP -p udp -m recent --update --seconds 60 --name UDP-PORTSCAN -j REJECT --reject-with icmp-port-unreachable
+iptables -D INPUT -p udp -j REJECT --reject-with icmp-port-unreachable
+iptables -A INPUT -p udp -m recent --set --name UDP-PORTSCAN -j REJECT --reject-with icmp-port-unreachable
+
+# Restore final stateful rule.
+iptables -D INPUT -j REJECT --reject-with icmp-proto-unreachable
+iptables -A INPUT -j REJECT --reject-with icmp-proto-unreachable
